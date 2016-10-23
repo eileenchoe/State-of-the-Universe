f6db7625f834f7feb809f1a2c8354f3d1ce1f5ac
Eileen Choe <echoe@lion.lmu.edu>
2016-10-22 19:45:25 -0700

    mars,apod,epic images

--- nasa.js
+++ nasa.js
@@ -2,20 +2,41 @@
 window.NASASearchController = (() => {
 	return {
 		init: () => {
-			var imageResultContainer = $(".image-result-container");
-			var image = $("#image");
+			var apodImage = $("#apodImage");
+			var curiosityImage = $("#curiosity");
+			var earthImage= $("#earth");
 			var text = $("#description");
 			$.getJSON("https://api.nasa.gov/planetary/apod" , {
 				date: "2016-10-20",
 				api_key:"yIzbNo0iFApOqbQS5BBpCH04hC3v3Opuwii8iw7Q",
 			}).done((result) => {
-				image.attr({
+				apodImage.attr({
 					src : result.hdurl,
 					alt : result.explanation,
 				});
-				text.text(result.explanation);
-				console.log(result.hdurl);
-				console.log(result.explanation);
+			});
+			$.getJSON("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos" , {
+				earth_date: "2016-10-20",
+				api_key: "yIzbNo0iFApOqbQS5BBpCH04hC3v3Opuwii8iw7Q",
+				camera: "NAVCAM",
+			}).done((result) => {
+				curiosityImage.attr({
+					src:result.photos[0].img_src,
+					alt:result.photos[0].full_name,
+				});
+				//console.log(result.photos[0].img_src);
+			});
+			$.getJSON("http://epic.gsfc.nasa.gov/api/images.php"
+				).done((result) => {
+					var imagePage="https://api.nasa.gov/EPIC/archive/natural/png/"+result[0].image+".png";
+					$.getJSON(imagePage,{
+						api_key: "yIzbNo0iFApOqbQS5BBpCH04hC3v3Opuwii8iw7Q",
+					}).done((result2) => {
+						var imageSource=result2 +"";
+						earthImage.attr({
+							src:imageSource,
+						});
+					});
 			});
 		}
 	}
